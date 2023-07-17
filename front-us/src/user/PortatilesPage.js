bb6d0aac2de287b5295eb29b95b27e96183b2cc8
AdrianBurgosA <aaburgos1@espe.edu.ec>
2023-07-17 10:52:48 -0500

    Conectado con el back end

--- front-us/src/user/PortatilesPage.js
+++ front-us/src/user/PortatilesPage.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Template from "./Template";
+import api from "../api/Axios";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Button, Card, CardActions, CardContent, Stack, Typography } from "@mui/material";
+import Divider from '@mui/material/Divider';
 
 const PortatilesPage = () => {
   const title = "Portátiles";
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/products');
+        setProducts(response.data)
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
+    };
+    fetchData()
+  }, []);
 
   return (
-    <Template title={title}>      
+    <Template title={title}>
       <br />
       <center>
         <h1>{title}</h1>
       </center>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          {products.map((p) => (
+            <Grid item xs>
+              <Card sx={{backgroundColor: "#EEEEEE", width: "max-content", padding: "20px", borderRadius: "5%"}}>
+                <CardContent>
+                  <Stack spacing={2} direction="row">
+                    <Typography>{p.name}</Typography>
+                    <Box sx={{ color: "orange", fontWeight: 'bold'}}>{p.price}</Box>
+                  </Stack>
+                  <Divider />
+                  <Typography>{p.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Stack spacing={2} direction="row">
+                    <Button variant="contained" sx={{backgroundColor: "#47555E", color: "#EEEEEE"}}>Agregar</Button>
+                  </Stack>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Template>
   );
 };
